e26dfe84dfcf27b1fb7c758c0b8fef87b6c7cf5b
nemoVim <nemovim@kentech.ac.kr>
2023-11-28 11:18:38 +0900

    Create model & modify frequency functions

--- clustering/frequency.py
+++ clustering/frequency.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 
 class FrequencyData:
-    def __init__(self, frequencyFolder='.', fileCount=250):
+    def __init__(self, fileName):
         self.data = {
             'recipeId': [],
             'frequency': [],
@@ -10,53 +10,47 @@ class FrequencyData:
             'sampleCnt': 0
         }
 
-        frequencyIdArr = []
-
-        with open(f'{frequencyFolder}/frequencyId.csv', encoding='utf-8') as frequencyIdData:
-            frequencyIdReader = csv.reader(frequencyIdData)
-            for _frequencyIdArr in frequencyIdReader:
-                frequencyIdArr = _frequencyIdArr
-
-
-        id = 0
-        for i in range(fileCount):
-            with open(f'{frequencyFolder}/frequency_{i}.csv', encoding='utf-8') as frequencyData:
-                frequencyReader = csv.reader(frequencyData)
-                for frequency in frequencyReader:
-                    self.data['recipeId'].append(frequencyIdArr[id])
-                    self.data['frequency'].append(self.decomopress(frequency))
-                    self.data['clusterId'].append(0)
-                    id += 1
-        
+        with open(fileName, encoding='utf-8') as f:
+            r = csv.reader(f)
+            for line in r:
+                if line[0] == 'id':
+                    continue
+                self.data['recipeId'].append(line[0])
+                self.data['frequency'].append(self.decomopress(line[1].split('/')))
+                self.data['clusterId'].append(0)
+    
         self.data['frequency'] = np.array(self.data['frequency'])
-        self.data['sampleCnt'] = id
 
     def decomopress(self, compressedVector):
         return list(map(int, list('1'.join(['0'*int(i) for i in compressedVector]))))
 
-    def clean(self, lessThan=1, greaterThan=500):
-        newIdArr = []
+    # def clean(self, lessThan=1):
+    #     newIdArr = []
 
-        maskArr = (self.data['frequency'].sum(axis=0) >= lessThan) & (self.data['frequency'].sum(axis=0) <= greaterThan)
+    #     # maskArr = (self.data['frequency'].sum(axis=0) >= lessThan) & (self.data['frequency'].sum(axis=0) <= greaterThan)
+    #     maskArr = (self.data['frequency'].sum(axis=0) >= lessThan)
 
-        self.data['frequency']
+    #     self.data['frequency']
 
-        for i in range(len(maskArr)):
-            if maskArr[i]:
-                newIdArr.append(i)
+    #     for i in range(len(maskArr)):
+    #         if maskArr[i]:
+    #             newIdArr.append(i)
 
-        self.data['frequency'] = self.data['frequency'][:, maskArr]
+    #     self.data['frequency'] = self.data['frequency'][:, maskArr]
 
-        # remove rows that have only zeros.
+    #     # remove rows that have only zeros.
 
-        return newIdArr
+    #     return newIdArr
 
 if __name__ == '__main__':
-    frequency= FrequencyData('../data-crawling/frequencyData', 250)
+    frequency= FrequencyData('../data-crawling/frequencyData/allFrequency_cleaned.csv')
+    
 
     print('Initialized')
-    print(frequency.data['frequency'].sum(axis=0)[86])
-    print(frequency.data['frequency'].sum(axis=0)[945])
+
+    print(frequency.data['frequency'][0])
+    # print(frequency.data['frequency'].sum(axis=0)[86])
+    # print(frequency.data['frequency'].sum(axis=0)[945])
 
 
 
